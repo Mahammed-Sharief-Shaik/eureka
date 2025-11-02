faaace2175c5d077de658aff4bab5a73d53513bd
Mahammed-Sharief-Shaik <learntech466@example.com>
2025-11-02 20:00:23 +0530

    loginpage signup page added

--- eureka-frontend/src/pages/Login.tsx
+++ eureka-frontend/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/shadcn/login-form"
 
 const Login = () => {
   return (
-    <LoginForm className="bg-bg-primary text-white"/>
+    <LoginForm className="bg-bg-primary text-white min-h-screen"/>
   )
 }
 
