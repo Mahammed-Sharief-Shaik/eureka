94ae1ddd32e44ec2bc92516a0f32e542972ce62c
Mahammed-Sharief-Shaik <learntech466@example.com>
2025-12-17 11:00:11 +0530

    fix 17-12

--- eureka-backend/src/db/getLastChats.ts
+++ eureka-backend/src/db/getLastChats.ts
@@ -1,5 +1,5 @@
 import prisma from "../prisma.js";
-import { MessageRole } from "@prisma/client/index.js";
+import { MessageRole } from "@prisma/client";
 
 type ChatMessage = {
   role: "system" | "user" | "assistant";
